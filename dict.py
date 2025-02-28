420d0d8b8dac033150ba357f9e4ac7195c4c8a93
sujan-18 <newarsujan2323@gmail.com>
2025-02-28 22:23:33 +0545

    add dictionary

---
+++ dict.py
@@ -0,0 +1,9 @@
+# practice dictionary from basic
+student_details={"name":"sujan","college":"lbef","Degree":"BSCIT","Age":21}
+print(type(student_details))
+print(student_details)
+
+# practice dictionary in another way
+student_details1=dict(name="sujan", college="lbef", Degree="BSCIT", Age=21)
+print(student_details1)
+
