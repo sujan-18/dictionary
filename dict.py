445bef06788203378e80b712bcf469af53ee9b4f
sujan-18 <newarsujan2323@gmail.com>
2025-02-28 23:04:17 +0545

    update code

--- dict.py
+++ dict.py
@@ -1,10 +1,19 @@
 # practice dictionary from basic
 student_details={"name":"sujan","college":"lbef","Degree":"BSCIT","Age":21}
-print(type(student_details))
-print(student_details)
+# print(type(student_details))
+# print(student_details)
 
 # practice dictionary in another way
 student_details1=dict(name="sujan", college="lbef", Degree="BSCIT", Age=21)
-print(student_details1)
 
-print(student_details["name"])
+# print(student_details["name"])
+# print(student_details.get("name"))
+x=student_details.keys()
+print(x)
+
+student_details["country"]="nepal"
+print(x)
+
+y=student_details.values()
+print(y)
+print("name" not in student_details)
